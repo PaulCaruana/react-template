bab996735b5a8f234d4b3b3aed3fec8822b94ef2
PaulCaruana <pgcaruana@optusnet.com.au>
2020-07-13 16:53:38 +1000

    tidy up

--- src/services/RestGateway.js
+++ src/services/RestGateway.js
@@ -2,7 +2,6 @@ import axios from "axios";
 
 const RestGateway = (endPoint) => {
     const fetchItem = async () => {
-        console.log("fetching..")
         const response = await axios.get(endPoint);
         return response;
     };
